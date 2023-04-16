2a6abcf80bb1d60714bfeb2bfe7522438ce8478b
alex <67538204+alexgmcss@users.noreply.github.com>
2023-04-16 09:35:59 +0200

    first commit

---
+++ NavBar.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { FaTwitter, FaGithub } from 'react-icons/fa';
+import styles from './NavBar.css';
+
+const Menu = () => {
+  return (
+    <div className={styles.menucontainer}>
+      <ul>
+        <li className={styles.menuitem2}>anmp. 👋</li>
+        <li className={styles.menuitem}><Link href="/">Home</Link></li>
+        <li className={styles.menuitem}><Link href="/aboutus">About</Link></li>
+        <li className={styles.menuitem}><Link href="#">Tools</Link></li>
+        <li className={styles.menuitem3}><Link href="https://twitter.com/heyanmp"><FaTwitter /></Link></li>
+        <li className={styles.menuitem3}><Link target="_blank" href="https://github.com/heyanmp"><FaGithub /></Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
